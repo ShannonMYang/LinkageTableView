87bc760efefa9058152561d95706d539b388be7f
ShannonMYang <myang.shannon1122@gmail.com>
2017-06-14 23:05:27 +0800

    实现双TableView的左右联动效果。

---
+++ TestLinkageTableView/TestLinkageTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestLinkageTableView
+//
+//  Created by ShannonMuY on 2017/6/14.
+//  Copyright © 2017年 MYang Shannon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestLinkageTableView/TestLinkageTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestLinkageTableView
+//
+//  Created by ShannonMuY on 2017/6/14.
+//  Copyright © 2017年 MYang Shannon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
